f3178adf214be93dfda7fd8bae7834bb68371aed
Anton <AimII@yandex.ru>
2017-12-02 17:12:20 +0600

    GameState

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public GameObject DeathPrefab = null;
+
+    InputManager _input = null;
+
+    Color _shipColor = Color.white;
+    int   _playerIndex = 0;
+
+    int _collectedMines = 0;
+
+    public bool Alive {
+        get {
+            return _isAlive;
+        }
+    }
+    bool _isAlive = true;
+
+    public void Init(int index, InputManager controls, Color color) {
+        _input = controls;
+        _playerIndex = index;
+        _shipColor = color;
+        UpdateModelColor();
+    }
+
+    public void Kill() {
+        if (!_isAlive) {
+            return;
+        }
+        //Spawn death prefab and etc
+    }
+
+    void UpdateModelColor() {
+        
+    }
+
+    void OnMineCollect() {
+
+    }
+
+    void OnDestroy() {
+
+    }
+}
