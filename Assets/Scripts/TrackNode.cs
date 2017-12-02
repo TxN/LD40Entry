af3503ab639523e76aa1fd079b523e08c31da548
rklipach <klipach.roman@gmail.com>
2017-12-02 19:35:20 +0700

    Fix for button "Add Track Node"

--- Assets/Scripts/TrackNode.cs
+++ Assets/Scripts/TrackNode.cs
@@ -13,8 +13,14 @@ public class TrackNode : MonoBehaviour {
 	public GameObject pole1;
 	public GameObject pole2;
 	private Vector2 pos;
+
 	public void Init(TrackNode prev)
 	{
+		// FIXME: it is workaround for first empty track-node
+		if (prev == null) {
+			return;
+		}
+
 		previous = prev;
 		position = new Vector2(previous.position.x + 1, previous.position.y + 1);
 		pos = new Vector2 (pole1Shift, 0);
@@ -25,6 +31,10 @@ public class TrackNode : MonoBehaviour {
 	}
 
 	void Update(){
+		// FIXME: it is workaround for first empty track-node
+		if (pole1 == null) {
+			return;
+		}
 		pole1.transform.localPosition.Set (pole1Shift, 0, 0);
 		pole2.transform.localPosition.Set (pole2Shift, 0, 0);
 	}
@@ -34,5 +44,7 @@ public class TrackNode : MonoBehaviour {
 		GameObject obj = new GameObject();
 		obj.AddComponent<TrackNode> ();
 		obj.name = "Track Node";
+		TrackNode trackNode = obj.GetComponent<TrackNode> () as TrackNode;
+		trackNode.Init (this);
 	}
 }
