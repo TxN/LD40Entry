e8950b40b1ed454edc4b85b5a4e4043a4ed66f79
rklipach <klipach.roman@gmail.com>
2017-12-02 19:11:41 +0700

    Add button for adding "Track Node" GameObject

--- Assets/Scripts/TrackNode.cs
+++ Assets/Scripts/TrackNode.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class TrackNode : MonoBehaviour {
 	public TrackNode previous;
 	public TrackNode next;
+
 	public Vector2 position;
 	public float pole1Shift;
 	public float pole2Shift;
+
 	public TrackNode(TrackNode prev)
 	{
 		previous = prev;
 		position = new Vector2 (previous.position.x + 1, previous.position.y + 1);
 		pole1Shift = 0.5f;
 		pole2Shift = 0.5f;
+	}
+
+	public void BuildObject()
+	{
+		GameObject obj = new GameObject();
+		obj.AddComponent<TrackNode> ();
+		obj.name = "Track Node";
 	}
 }
---
+++ Assets/Scripts/TrackNodeInspector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TrackNode))]
+public class TrackNodeInspector : Editor {
+
+	public override void OnInspectorGUI () {
+		DrawDefaultInspector();
+
+		TrackNode myScript = (TrackNode)target;
+		if(GUILayout.Button("Add Track Node"))
+		{
+			myScript.BuildObject();
+		}
+	}
+}
