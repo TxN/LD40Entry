bd4afaf793e2aaff115cc513e05e95d1b106e8e7
rklipach <klipach.roman@gmail.com>
2017-12-02 16:51:57 +0700

    Add Players from Lobby to PlayersInfoHolder

--- Assets/Scripts/Lobby.cs
+++ Assets/Scripts/Lobby.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lobby : MonoBehaviour {
+	public GameObject playersInfosGameObject;
+
+	private List<string> joinKeys = new List <string>() {"key1"}; //TODO
+	private List<string> joinKeysPrefixes = new List <string>() {"prefix1"}; //TODO
 
 	// Use this for initialization
 	void Start () {
-		
+		playersInfosGameObject = new GameObject();
+		playersInfosGameObject.AddComponent <PlayerInfoHolder>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		PlayerInfoHolder holder = playersInfosGameObject.GetComponent(typeof(PlayerInfoHolder)) as PlayerInfoHolder;
+
+		int i = 0;
+		while (i < joinKeys.Count) {
+			if (Input.GetKeyDown (joinKeys [i])) {
+				holder.AddPlayerInfo (new PlayerInfo (Color.black, joinKeysPrefixes [i])); //TODO: color
+				joinKeys.RemoveAt (i);
+				joinKeysPrefixes.RemoveAt (i);
+				i -= 1;
+			}
+
+			i += 1;
+		}
 	}
 }
---
+++ Assets/Scripts/PlayerInfoHolder.cs
@@ -0,0 +1,16 @@
+﻿using EventSys;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfoHolder : MonoBehaviour {
+	public List<PlayerInfo> playersInfos;
+
+	public void AddPlayerInfo(PlayerInfo player) {
+		playersInfos.Add(player);
+	}
+
+	void Start() {
+		DontDestroyOnLoad (this);
+	}
+}
