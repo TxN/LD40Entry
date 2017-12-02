71ee7b1f892f7eb95b00529cb10177ade591fe7c
Eugene <evg_kod@mail.ru>
2017-12-02 17:38:41 +0700

    Track base

---
+++ Assets/Scripts/TrackNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackNode : MonoBehaviour {
+	public TrackNode previous;
+	public TrackNode next;
+	public Vector2 position;
+	public float pole1Shift;
+	public float pole2Shift;
+	public TrackNode(TrackNode prev)
+	{
+		previous = prev;
+		position = new Vector2 (previous.position.x + 1, previous.position.y + 1);
+		pole1Shift = 0.5f;
+		pole2Shift = 0.5f;
+	}
+}
