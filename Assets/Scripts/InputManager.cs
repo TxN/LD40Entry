758a6b1904fb1ea2518aa6cb54d6b130357293ab
rklipach <klipach.roman@gmail.com>
2017-12-02 15:35:40 +0700

    InputManager
    LaunchDirection + MoveAcceleration + LaunchTrigger

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,6 +7,9 @@ public class InputManager : MonoBehaviour {
 	private string _directionAxisY;
 	private string _launchAxisX;
 	private string _launchAxisY;
+	private string _moveTrigger;
+	private string _backMoveTrigger; //TODO
+	private string _launchTrigger;
 
 	public void Init(string prefix)
 	{
@@ -14,15 +17,29 @@ public class InputManager : MonoBehaviour {
 		_directionAxisY = prefix + "_direction_y";
 		_launchAxisX = prefix + "_launch_x";
 		_launchAxisY = prefix + "_launch_y";
+		_moveTrigger = prefix + "_move";
+		_launchTrigger = prefix + "_launch";
 	}
 
 	public float GetDirection (){
 		Vector2 vec = new Vector2(Input.GetAxis(_directionAxisX), Input.GetAxis(_directionAxisY));
 		return Mathf.Atan2 (vec.y, vec.x);
 	}
-	public  float GetLaunchDirection(){return 0f;}
-	public  float GetMoveAcceleration (){return 0f;}
-	public  bool GetLaunchTrigger() {
-		return false;
+
+	public  float GetLaunchDirection(){
+		Vector2 vec = new Vector2(Input.GetAxis(_launchAxisX), Input.GetAxis(_launchAxisY));
+		return Mathf.Atan2 (vec.y, vec.x);
+	}
+
+	public float GetMoveAcceleration (){
+		if (Input.GetKeyDown (_moveTrigger)) {
+			return 1f;
+		}
+
+		return 0f;
+	}
+
+	public bool GetLaunchTrigger() {
+		return Input.GetKeyDown (_launchTrigger);
 	}
 }
