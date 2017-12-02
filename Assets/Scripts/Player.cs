a65cff9fb39a0eb2a16f0b45f50d788c1af57102
TxN <AimII@yandex.ru>
2017-12-03 01:53:25 +0700

    Grafon 101

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using EventSys;
 
 public class Player : MonoBehaviour {
     const float ROT_SMOOTH_COEF = 0.8f;
-    const float MAX_ACCELERATION = 10f;
+    const float MAX_ACCELERATION = 5f;
     const float MINE_DECC_PERCENT = 0.1f;
 	const float MINE_LAUNCH_COOLDOWN = 3f;
 
