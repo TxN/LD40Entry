205f40a61b870d87d7f9b9af18ce610c38df1612
Eugene <evg_kod@mail.ru>
2017-12-02 15:27:58 +0700

    InputManagerAgain

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,9 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class InputManager : MonoBehaviour {
-	public abstract float GetDirection ();
-	public abstract float GetLaunchDirection();
-	public abstract float GetMoveAcceleration ();
-	public abstract bool GetLaunchTrigger();
+public class InputManager : MonoBehaviour {
+	private string _directionAxisX;
+	private string _directionAxisY;
+	private string _launchAxisX;
+	private string _launchAxisY;
+
+	public void Init(string prefix)
+	{
+		_directionAxisX = prefix + "_direction_x";
+		_directionAxisY = prefix + "_direction_y";
+		_launchAxisX = prefix + "_launch_x";
+		_launchAxisY = prefix + "_launch_y";
+	}
+
+	public float GetDirection (){
+		Vector2 vec = new Vector2(Input.GetAxis(_directionAxisX), Input.GetAxis(_directionAxisY));
+		return Mathf.Atan2 (vec.y, vec.x);
+	}
+	public  float GetLaunchDirection(){return 0f;}
+	public  float GetMoveAcceleration (){return 0f;}
+	public  bool GetLaunchTrigger() {
+		return false;
+	}
 }
