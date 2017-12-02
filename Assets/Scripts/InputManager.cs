a8583dc4b696f8a0b58d361a88e0b9567ba6841d
Eugene <evg_kod@mail.ru>
2017-12-02 14:19:47 +0700

    Input Manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputManager : MonoBehaviour {
+	public abstract float GetDirection ();
+	public abstract float GetLaunchDirection();
+	public abstract float GetMoveAcceleration ();
+	public abstract bool GetLaunchTrigger();
+}
