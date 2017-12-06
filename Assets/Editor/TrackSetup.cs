a4c2e8cf93c8f08769e92700eb12a3142fd5ae95
TxN <AimII@yandex.ru>
2017-12-07 02:46:38 +0700

    Тестовая генерация поверхности пола трассы.

--- Assets/Editor/TrackSetup.cs
+++ Assets/Editor/TrackSetup.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
 
 public class TrackSetup : MonoBehaviour {
@@ -6,6 +7,7 @@ public class TrackSetup : MonoBehaviour {
 	public static void SetupTrack() {
         var TrackHolder = FindObjectOfType<RacetrackHolder>();
 		var nodes = FindObjectsOfType<TrackNode>();
+		
 		foreach (var node in nodes) {
 			if (node.next == null) {
 				continue;
@@ -27,8 +29,6 @@ public class TrackSetup : MonoBehaviour {
             col.points = new Vector2[] { pole1tr.localPosition, pole1tr.InverseTransformPoint(node.next.pole1.transform.GetChild(0).position) };
             col = null;
 
-            
-
 			ren = node.pole2.GetComponentInChildren<LineRenderer>();
 			points[0] = node.pole2.transform.GetChild(0).position;
 			points[1] = node.next.pole2.transform.GetChild(0).position;
@@ -43,5 +43,77 @@ public class TrackSetup : MonoBehaviour {
             col.points = new Vector2[] { pole2tr.localPosition, pole2tr.InverseTransformPoint(node.next.pole2.transform.GetChild(0).position) };
             col = null;
 		}
+	
+	/*	MeshFilter filter = TrackHolder.GetComponent<MeshFilter>();
+		if ( !filter ) {
+			filter = TrackHolder.gameObject.AddComponent<MeshFilter>();
+			if ( !TrackHolder.gameObject.GetComponent<MeshRenderer>()) {
+				TrackHolder.gameObject.AddComponent<MeshRenderer>();
+			}
+		}
+
+		filter.sharedMesh = DoMesh(TrackHolder);
+		*/
+	}
+
+	static Mesh DoMesh(RacetrackHolder TrackHolder) {
+		
+		Mesh trackFloor = new Mesh();
+		trackFloor.name = TrackHolder.TrackName;
+		List<Vector3> trackVerts = new List<Vector3>(512);
+		List<Vector3> trackNormals = new List<Vector3>(512);
+		List<Vector2> trackUVs = new List<Vector2>(512);
+		List<int> trackIndices = new List<int>(512);
+		int indiceIndex = 0;
+
+		TrackNode node = TrackHolder.FirstNode;
+		do {
+			trackVerts.Add(node.pole1.transform.position);
+			trackVerts.Add(node.previous.pole1.transform.position);
+			trackVerts.Add(node.pole2.transform.position);
+			trackUVs.Add(new Vector2(0, 1));		
+			trackUVs.Add(new Vector2(0, 0));
+			trackUVs.Add(new Vector2(1, 1));
+			var normal = Vector3.Cross((trackVerts[indiceIndex + 1] - trackVerts[indiceIndex]), (trackVerts[indiceIndex + 2] - trackVerts[indiceIndex])).normalized;
+			trackNormals.Add(normal); 
+			trackNormals.Add(normal);
+			trackNormals.Add(normal);
+
+			trackVerts.Add(node.previous.pole2.transform.position);
+			trackVerts.Add(node.pole2.transform.position);
+			trackVerts.Add(node.previous.pole1.transform.position);
+			
+
+			trackUVs.Add(new Vector2(1, 0));
+			trackUVs.Add(new Vector2(1, 1));
+			trackUVs.Add(new Vector2(0, 0));
+			
+			normal = Vector3.Cross((trackVerts[indiceIndex + 4] - trackVerts[indiceIndex + 3]), (trackVerts[indiceIndex + 5] - trackVerts[indiceIndex + 3])).normalized;
+			trackNormals.Add(normal);
+			trackNormals.Add(normal);
+			trackNormals.Add(normal);
+
+			trackIndices.Add(indiceIndex);
+			trackIndices.Add(indiceIndex + 1);
+			trackIndices.Add(indiceIndex + 2);
+			trackIndices.Add(indiceIndex + 3);
+			trackIndices.Add(indiceIndex + 4);
+			trackIndices.Add(indiceIndex + 5);
+
+			indiceIndex += 6;
+
+			node = node.next;
+		} while (node != TrackHolder.FirstNode);
+
+		trackFloor.SetVertices(trackVerts);
+		trackFloor.SetNormals(trackNormals);
+		trackFloor.SetUVs(0, trackUVs);
+		trackFloor.SetIndices(trackIndices.ToArray(), MeshTopology.Triangles, 0);
+		trackFloor.SetTriangles(trackIndices.ToArray(), 0);
+		trackFloor.RecalculateBounds();
+		trackFloor.RecalculateNormals();
+		trackFloor.RecalculateTangents();
+		trackFloor.UploadMeshData(false);
+		return trackFloor;
 	}
 }
--- Assets/Scripts/RacetrackHolder.cs
+++ Assets/Scripts/RacetrackHolder.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
 public class RacetrackHolder : MonoBehaviour {
+	public string    TrackName = "";
     public TrackNode FirstNode = null;
 }
